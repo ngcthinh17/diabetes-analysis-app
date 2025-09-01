b2c4d976feabd4e13dc1bc95940ea5857df5725e
Nguyễn Công Thịnh <nguyencongthinh17122006@gmail.com>
2025-09-01 22:37:16 +0700

    Update run_app.bat

--- scripts/run_app.bat
+++ scripts/run_app.bat
@@ -2,4 +2,22 @@
 python -m venv .venv
 call .venv\Scripts\activate
 pip install -r requirements.txt
+
+@REM Windows – CMD/PowerShell (đứng ở thư mục gốc repo diabetes-analysis-app):
+
+cd cd path\to\diabetes-analysis-app
+set PYTHONPATH=%CD%
 streamlit run app\main.py
+
+@REM PowerShell (nếu dùng PS):
+
+cd path\to\diabetes-analysis-app
+$env:PYTHONPATH = (Get-Location).Path
+streamlit run app\main.py
+
+
+@REM macOS/Linux:
+
+cd /path/to/diabetes-analysis-app
+export PYTHONPATH=$(pwd)
+streamlit run app/main.py
